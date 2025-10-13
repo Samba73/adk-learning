c7fedc3252de98b5acb9b653467ac610df63b2b9
Sambasivam Krishnamurthy <smbkrishnamurthy@gmail.com>
2025-10-13 11:37:59 +0530

    fixed agent name in runner call

--- 9-callbacks/before_after_tool/agent.py
+++ 9-callbacks/before_after_tool/agent.py
@@ -79,7 +79,7 @@ async def setup_session_runner():
     session_service = InMemorySessionService()
     session = await session_service.create_session(
         app_name=APP_NAME,user_id=USER_ID,session_id=SESSION_ID)
-    runner = Runner(agent =llm_agent, app_name=APP_NAME, session_service=session_service)
+    runner = Runner(agent =root_agent, app_name=APP_NAME, session_service=session_service)
     return session, runner
 
 async def call_agent_async(query):
